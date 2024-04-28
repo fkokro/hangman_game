cd90ad9d1489217f1e616fc3ee3a4d5546eabc70
Flomo Kokro <fkokro@yahoo.com>
2024-04-28 00:16:05 -0400

    Framework complete

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "hangman_game"
+AUTHOR_USER_NAME = "fkokro"
+SRC_REPO = "HangManSolverProject"
+AUTHOR_EMAIL = "fkokro@yahoo.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A application developed to solve hangman game leveraging machine learning; Transformer and/or BERT MODEL.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+    
+)
